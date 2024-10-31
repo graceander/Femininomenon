5d6d0cbe735ddbf62557b3d063bd893a3b7f5ba2
Briana Weik <briana.weik@wsu.edu>
2024-10-30 20:36:38 -0700

    Implemented remaining tests for the STModel

--- src/test/java/org/cpts422/Femininomenon/App/Models/ScheduledTransactionModelTest.java
+++ src/test/java/org/cpts422/Femininomenon/App/Models/ScheduledTransactionModelTest.java
@@ -1,5 +1,7 @@
 package org.cpts422.Femininomenon.App.Models;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
@@ -12,8 +14,12 @@ public class ScheduledTransactionModelTest {
     @Mock
     UserModel userModel;
     @InjectMocks
-    ScheduledTransactionModel scheduledTransactionModel;
+    ScheduledTransactionModel scheduledTransaction;
 
+    @BeforeEach
+    void setUp() {
+        scheduledTransaction = new ScheduledTransactionModel(userModel, "Weekly", LocalDateTime.now(), 1.0f, ScheduledTransactionModel.CategoryType.GROCERIES, "test", ScheduledTransactionModel.TransactionType.EXPENSE, "checking");
+    }
     @Test
     void testDefaultConstructor() {
         ScheduledTransactionModel scheduledTransactionModel = new ScheduledTransactionModel();
@@ -32,5 +38,62 @@ public class ScheduledTransactionModelTest {
             assertEquals(ScheduledTransactionModel.TransactionType.EXPENSE, scheduledTransactionModel.getType());
             assertEquals("checking", scheduledTransactionModel.getAccount());
         });
+    }
+
+    @Nested
+    class testSetters {
+        @Test
+        void testSetID() {
+            assertDoesNotThrow(() -> scheduledTransaction.setId(1L));
+            assertEquals(1L, scheduledTransaction.getId());
+        }
+
+        @Test
+        void testSetUser() {
+            assertDoesNotThrow(() -> scheduledTransaction.setUser(userModel));
+            assertEquals(userModel, scheduledTransaction.getUser());
+        }
+
+        @Test
+        void testSetFrequency() {
+            assertDoesNotThrow(() -> scheduledTransaction.setFrequency("Monthly"));
+            assertEquals("Monthly", scheduledTransaction.getFrequency());
+        }
+
+        @Test
+        void testSetRecentPayment() {
+            assertDoesNotThrow(() -> scheduledTransaction.setRecentPayment(LocalDateTime.now()));
+            assertEquals(LocalDateTime.now(), scheduledTransaction.getRecentPayment());
+        }
+
+        @Test
+        void testSetAmount() {
+            assertDoesNotThrow(() -> scheduledTransaction.setAmount(1.0f));
+            assertEquals(1.0f, scheduledTransaction.getAmount());
+        }
+
+        @Test
+        void testSetDescription() {
+            assertDoesNotThrow(() -> scheduledTransaction.setDescription("test"));
+            assertEquals("test", scheduledTransaction.getDescription());
+        }
+
+        @Test
+        void testSetCategory() {
+            assertDoesNotThrow(() -> scheduledTransaction.setCategory(ScheduledTransactionModel.CategoryType.GROCERIES));
+            assertEquals(ScheduledTransactionModel.CategoryType.GROCERIES, scheduledTransaction.getCategory());
+        }
+
+        @Test
+        void testSetTransactionType() {
+            assertDoesNotThrow(() -> scheduledTransaction.setType(ScheduledTransactionModel.TransactionType.EXPENSE));
+            assertEquals(ScheduledTransactionModel.TransactionType.EXPENSE, scheduledTransaction.getType());
+        }
+
+        @Test
+        void testSetAccount() {
+            assertDoesNotThrow(() -> scheduledTransaction.setAccount("Savings"));
+            assertEquals("Savings", scheduledTransaction.getAccount());
+        }
     }
 }
