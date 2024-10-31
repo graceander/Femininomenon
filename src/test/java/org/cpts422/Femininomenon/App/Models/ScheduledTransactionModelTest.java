651c8294956f49d33fe2b678d1d57004d0ea345b
Briana Weik <briana.weik@wsu.edu>
2024-10-30 17:06:04 -0700

    Implemented most of the tests for the STModel; just need to test setters.

--- src/test/java/org/cpts422/Femininomenon/App/Models/ScheduledTransactionModelTest.java
+++ src/test/java/org/cpts422/Femininomenon/App/Models/ScheduledTransactionModelTest.java
@@ -1,4 +1,36 @@
 package org.cpts422.Femininomenon.App.Models;
 
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+
+import java.time.LocalDateTime;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 public class ScheduledTransactionModelTest {
+    @Mock
+    UserModel userModel;
+    @InjectMocks
+    ScheduledTransactionModel scheduledTransactionModel;
+
+    @Test
+    void testDefaultConstructor() {
+        ScheduledTransactionModel scheduledTransactionModel = new ScheduledTransactionModel();
+        assertNotNull(scheduledTransactionModel);
+    }
+    @Test
+    void testParameterizedConstructor() {
+        ScheduledTransactionModel scheduledTransactionModel = new ScheduledTransactionModel(userModel, "Weekly", LocalDateTime.now(), 1.0f, ScheduledTransactionModel.CategoryType.GROCERIES, "test", ScheduledTransactionModel.TransactionType.EXPENSE, "checking");
+        assertAll( () -> {
+            assertEquals(userModel, scheduledTransactionModel.getUser());
+            assertEquals("Weekly", scheduledTransactionModel.getFrequency());
+            assertNotNull(scheduledTransactionModel.getRecentPayment());
+            assertEquals(1.0f, scheduledTransactionModel.getAmount());
+            assertEquals("Scheduled Transaction: test", scheduledTransactionModel.getDescription());
+            assertEquals(ScheduledTransactionModel.CategoryType.GROCERIES, scheduledTransactionModel.getCategory());
+            assertEquals(ScheduledTransactionModel.TransactionType.EXPENSE, scheduledTransactionModel.getType());
+            assertEquals("checking", scheduledTransactionModel.getAccount());
+        });
+    }
 }
